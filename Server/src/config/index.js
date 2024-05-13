0b033ef6650c048600d348c1506598f3d36435d4
Bhaskar <bhaskersonowal12@gmail.com>
2024-05-14 01:19:43 +0530

    n1

--- Server/src/config/index.js
+++ Server/src/config/index.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI = process.env.MONGODB_URI;
 // Add other configurations or environment variables as needed
